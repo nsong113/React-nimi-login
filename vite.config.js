97127203fa2190b7da41958adb5498e37a8d036c
nsong113@gmail.com <nsong113@gamil.com>
2023-11-15 21:25:01 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // 빌드된 파일이 저장될 디렉토리 설정
+    outDir: "build", // 빌드된 파일이 저장될 디렉토리 설정
   },
 });
